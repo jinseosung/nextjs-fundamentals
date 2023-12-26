6a8b28dae45b3f7c711d68d0423f8b837eefc9cd
Jinseo Sung <jinseosung15@gmail.com>
2023-12-26 15:46:48 +0100

    create NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const NavBar: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/vercel.svg" alt="logo" width={100} height={25} />
+      </div>
+      <div className="links">
+        <Link href="/" style={{ textDecoration: "none" }}>
+          <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+        </Link>
+        <Link href="/about" style={{ textDecoration: "none" }}>
+          <span className={router.pathname === "/about" ? "active" : ""}>
+            About
+          </span>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        .logo {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        span {
+          color: tomato;
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .links {
+          display: flex;
+          gap: 10px;
+        }
+        .active {
+          color: white;
+        }
+      `}</style>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Seo from "@/components/Seo";
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <>
+      <Seo title="About" />
+      <main>
+        <h1>About</h1>
+      </main>
+    </>
+  );
+};
+
+export default About;
